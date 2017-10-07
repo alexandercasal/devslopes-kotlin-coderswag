550126e4d54dc89e24bd78acfc11ebdceb1ebd65
Alexander Casal <alexandericasal@gmail.com>
2017-10-07 17:33:16 -0600

    lesson 02. Added list view adapter and structure for list item view

--- app/src/main/java/com/alexandercasal/devslopes/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/alexandercasal/devslopes/coderswag/controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.alexandercasal.devslopes.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.alexandercasal.devslopes.coderswag.R
+import com.alexandercasal.devslopes.coderswag.model.Category
+import com.alexandercasal.devslopes.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories
+        )
+        list_categories.adapter = adapter
     }
 }
--- app/src/main/java/com/alexandercasal/devslopes/coderswag/model/Category.kt
+++ app/src/main/java/com/alexandercasal/devslopes/coderswag/model/Category.kt
@@ -3,4 +3,8 @@ package com.alexandercasal.devslopes.coderswag.model
 /**
  * Created by Alexander on 10/7/2017.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}

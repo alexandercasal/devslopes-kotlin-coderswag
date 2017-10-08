3078769e0b53665e5eb9700e6cb2e4bcffaff01e
Alexander Casal <alexandericasal@gmail.com>
2017-10-07 21:05:06 -0600

    refactored for recycler view

---
+++ app/src/main/java/com/alexandercasal/devslopes/coderswag/adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,49 @@
+package com.alexandercasal.devslopes.coderswag.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.alexandercasal.devslopes.coderswag.R
+import com.alexandercasal.devslopes.coderswag.model.Category
+import kotlinx.android.synthetic.main.category_list_item.view.*
+
+/**
+ * Created by Alexander on 10/7/2017.
+ */
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>)
+    : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    /**
+     * Prepares child view to display info corresponding to its position in the adapter
+     */
+    override fun onBindViewHolder(holder: Holder?, position: Int) {
+        holder?.bindCategory(context, categories[position])
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.category_list_item, parent, false)
+        return Holder(view)
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView?.findViewById<ImageView>(R.id.image_background)
+        val categoryName = itemView?.findViewById<TextView>(R.id.text_label)
+
+        fun bindCategory(context: Context, category: Category) {
+            val resourceID = context.resources.getIdentifier(category.image,
+                    "drawable", context.packageName
+            )
+            categoryImage?.setImageResource(resourceID)
+            categoryName?.text = category.title
+        }
+    }
+}
--- app/src/main/java/com/alexandercasal/devslopes/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/alexandercasal/devslopes/coderswag/controller/MainActivity.kt
@@ -2,29 +2,27 @@ package com.alexandercasal.devslopes.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
 import android.widget.Toast
 import com.alexandercasal.devslopes.coderswag.R
 import com.alexandercasal.devslopes.coderswag.adapters.CategoryAdapter
+import com.alexandercasal.devslopes.coderswag.adapters.CategoryRecycleAdapter
 import com.alexandercasal.devslopes.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(
-                this,
-                DataService.categories
-        )
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
         list_categories.adapter = adapter
-
-        list_categories.setOnItemClickListener { adapterView, view, i, l ->
-            val category = DataService.categories[i]
-            Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
-        }
+        val layoutManager = LinearLayoutManager(this)
+        list_categories.layoutManager = layoutManager
+        list_categories.setHasFixedSize(true)
     }
 }
